1ed0aec6d96d1e7603a9430e0a6f1f3081508628
neoRakesh <neo3computer@gmail.com>
2023-09-16 15:17:58 +0530

    admin folder and shop folder with files created

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product=require('../models/product');
 
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -18,13 +18,16 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products=Product.fetchAll();
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll(products =>{
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+
   });
+ 
 };
--- models/product.js
+++ models/product.js
@@ -1,16 +1,51 @@
-const products=[];
+const path = require('path');
+const fs = require('fs');
 
-module.exports= class Products{
-    constructor(t){
-        this.title=t;
+const p = path.join(path.dirname(require.main.filename),
+    'data',
+    'products.json'
+);
+
+//const products = [];
+
+//helper function
+const getProductsFromFile = cb => {
+
+    var json;
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        else {
+            json = JSON.parse(fileContent);
+            cb(json);
+        }
+    })
+}
+
+
+module.exports = class Products {
+    constructor(t) {
+        this.title = t;
 
     }
 
-    save(){
-        products.push(this)
+    save() {
+        // products.push(this)
+
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            })
+        })
     }
 
-    static fetchAll(){
-        return products;
+
+    static fetchAll(cb) {
+
+        getProductsFromFile(cb);
+
     }
 }
